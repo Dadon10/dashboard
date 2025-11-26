75b7f34c92579702264999e0a77d3035c0e53ffd
Mike Mambwe <mikeapple056@gmail.com>
2025-11-26 12:11:08 +0200

    config and auth fixes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,23 +1,31 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-let app, auth, db;
+let app, auth, db, storage;
 
 export const initFirebase = () => {
-  const configStr = localStorage.getItem('firebaseConfig');
-  if (!configStr) return false;
-
+  if (app) return true; 
   try {
-    const config = JSON.parse(configStr);
-    app = initializeApp(config);
+    const firebaseConfig = {
+      apiKey: "AIzaSyAWywzGnsKCkfuabo7hCpzECkizVUlwFtc",
+      authDomain: "garbagecollectionapp-e999e.firebaseapp.com",
+      projectId: "garbagecollectionapp-e999e",
+      storageBucket: "garbagecollectionapp-e999e.appspot.com",
+      messagingSenderId: "34201078156",
+      appId: "1:34201078156:web:befecd59c9c99f9357aea9",
+      measurementId: "G-BSDLHWCKJX",
+    };
+    app = initializeApp(firebaseConfig);
     auth = getAuth(app);
     db = getFirestore(app);
+    storage = getStorage(app);
     return true;
   } catch (err) {
-    console.error('Firebase initialization error:', err);
+    console.error("Firebase initialization error:", err);
     return false;
   }
 };
 
-export { app, auth, db };
+export { app, auth, db, storage };
